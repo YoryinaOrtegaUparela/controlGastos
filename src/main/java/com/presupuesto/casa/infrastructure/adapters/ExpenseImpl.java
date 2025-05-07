cfd0ab190dcc23b523eb9c8bd90530111fa018b0
JAIRO <beltranjda@gmail.com>
2025-05-06 21:26:51 -0500

    Base del proyecto ok, con conexión a la base de datos en memoria.

--- src/main/java/com/presupuesto/casa/infrastructure/adapters/ExpenseImpl.java
+++ src/main/java/com/presupuesto/casa/infrastructure/adapters/ExpenseImpl.java
@@ -2,9 +2,6 @@ package com.presupuesto.casa.infrastructure.adapters;
 
 import com.presupuesto.casa.application.usecases.ports.output.ExpensePort;
 import com.presupuesto.casa.domain.models.Expense;
-import com.presupuesto.casa.infrastructure.entity.ExpenseEntity;
-import com.presupuesto.casa.infrastructure.mappers.MapperExpense;
-import com.presupuesto.casa.infrastructure.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
@@ -13,28 +10,18 @@ import java.util.List;
 @Service
 public class ExpenseImpl implements ExpensePort {
 
-    @Autowired
-    private ExpenseRepository expenseRepository;
-
-    @Autowired
-    private MapperExpense mapperExpense;
-
     @Override
     public List<Expense> getExpensesByHomeIdForDate(Long homeId, LocalDate initDate, LocalDate endDate) {
-        List<ExpenseEntity> listExpenseEntity =
-                expenseRepository.findByHomeIdAndExecuteExpenseDateBetween(homeId, initDate, endDate);
-        return mapperExpense.expenseEntityListToExpenseList(listExpenseEntity);
+        return List.of();
     }
 
     @Override
     public Expense saveExpense(Expense expense) {
-        ExpenseEntity expenseEntity =
-                expenseRepository.saveExpense(mapperExpense.expenseToExpenseEntity(expense));
-        return mapperExpense.expenseEntityToExpense(expenseEntity);
+        return null;
     }
 
     @Override
     public void deleteExpense(Long id) {
-        expenseRepository.deleteExpenseById(id);
+
     }
 }
